cd145e69511a9ae99d70d39f32c957973ced10e6
Zane Kissel <zekissel@gmail.com>
2022-11-09 15:23:15 -0500

    different tests

--- hashset/test.c
+++ hashset/test.c
@@ -8,10 +8,11 @@ int main () {
     test = put(test, "test", 10);
     test = put(test, "test2", 20);
     test = put(test, "hashset", 24);
-    test = put(test, "birmingham", 69);
-    test = put(test, "hell", 666);
-    test = put(test, "weed", 420);
+    test = put(test, "birmingham", 71);
+    test = put(test, "hell6", 6660);
+    test = put(test, "frost", 430);
     test = put(test, "horoscope", 13);
+    test = put(test, "greenthumb", 42);
 
     printf("Entries: %d\n" + size(test));
     
